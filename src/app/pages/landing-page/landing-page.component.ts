27ca445725571fe13958bf2e2a181e10f195645b
Muhammad Hassan <muhammad.hassan@secp.gov.pk>
2022-06-03 09:36:06 +0500

    modified help section

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { ROUTES_URL } from '../../enums/routes.enum'
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent {}
+export class LandingPageComponent {
+  readonly helpUrl = ROUTES_URL.HELP_URL;
+}
